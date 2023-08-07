904c13145b17a46ca692389ae29c14bbe558dedb
ElisaSoruco <elisasoruco28@gmail.com>
2023-08-07 09:47:20 -0300

    Cambios

---
+++ app.js
@@ -0,0 +1,40 @@
+
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+app.use(express.static('public'));
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`Server up:  PORT:${PORT}`));
+
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/home.html'))
+})
+
+app.get('/product', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/product.html'))
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+});
+
+app.post('/register', (req, res) => {
+    res.redirect('/')
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+});
+
+app.post('/login', (req, res) => {
+    res.redirect('/home')
+});
+
+
+
+
+
